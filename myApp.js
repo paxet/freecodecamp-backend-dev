26af12e8be313680cd8217ef41b574f43146aa78
Javier Caballero <jacallo@protonmail.com>
2023-10-06 23:37:28 +0200

    feat: serve /json

--- myApp.js
+++ myApp.js
@@ -10,6 +10,10 @@ app.get("/", function(req, res) {
 app.use("/public", express.static(__dirname + "/public"));
 
 
+app.get("/json", function(req, res) {
+  res.json({ message: "Hello json" });
+});
+
 
 
 
