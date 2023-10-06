6c527dd64e79ee914547d8b526a3b7926d3e90b1
Javier Caballero <jacallo@protonmail.com>
2023-10-06 23:55:58 +0200

    feat: read var from env

--- myApp.js
+++ myApp.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 let express = require('express');
 let app = express();
 
@@ -11,7 +12,10 @@ app.use("/public", express.static(__dirname + "/public"));
 
 
 app.get("/json", function(req, res) {
-  res.json({ message: "Hello json" });
+  mess = "Hello json";
+  if (process.env.MESSAGE_STYLE == "uppercase")
+    mess = mess.toUpperCase();
+  res.json({ message: mess });
 });
 
 
