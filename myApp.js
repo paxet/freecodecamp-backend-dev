0c36bf2507a8783384cfbe486a25c123354a9afa
Javier Caballero <jacallo@protonmail.com>
2023-10-06 23:17:51 +0200

    feat: serve views/index.html

--- myApp.js
+++ myApp.js
@@ -2,7 +2,9 @@ let express = require('express');
 let app = express();
 
 app.get("/", function(req, res) {
-  res.send("Hello Express");
+  // res.send("Hello Express");
+  absolutePath = __dirname + '/views/index.html';
+  res.sendFile(absolutePath);
 });
 
 
