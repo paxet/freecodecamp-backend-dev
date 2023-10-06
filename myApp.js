3ec99ec7c689a45447b1f3975e7f398de1ac8e74
Javier Caballero <jacallo@protonmail.com>
2023-10-07 00:17:43 +0200

    feat: middleware to log method, path and ip

--- myApp.js
+++ myApp.js
@@ -2,14 +2,20 @@ require('dotenv').config();
 let express = require('express');
 let app = express();
 
+// MIDDLEWARE
+app.use("/public", express.static(__dirname + "/public"));
+
+app.use(function(req, res, next) {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next();
+});
+
+// ENDPOINTS
 app.get("/", function(req, res) {
   // res.send("Hello Express");
   absolutePath = __dirname + '/views/index.html';
   res.sendFile(absolutePath);
 });
-
-app.use("/public", express.static(__dirname + "/public"));
-
 
 app.get("/json", function(req, res) {
   mess = "Hello json";
@@ -17,21 +23,6 @@ app.get("/json", function(req, res) {
     mess = mess.toUpperCase();
   res.json({ message: mess });
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
