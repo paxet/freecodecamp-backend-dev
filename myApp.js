2c98e1aae8ea5cfd601c1e38605c445302e1586e
Javier Caballero <jacallo@protonmail.com>
2023-10-07 10:16:18 +0200

    feat: endpoint /name for GET

--- myApp.js
+++ myApp.js
@@ -35,6 +35,10 @@ app.get("/:word/echo", function(req, res) {
   res.json({ echo: req.params.word });
 });
 
+app.route("/name").get(function(req, res) {
+  res.json({ name: `${req.query.first} ${req.query.last}` });
+});
+
 
 
 
